ccf3141019d6ccf23cbca735845fcba3dd910c84
Martin Lerch <lerch.martin@ssakhk.cz>
2021-03-24 23:45:31 +0100

    Basic date generator

---
+++ DateGenerator/DateExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DateGenerator;
+
+namespace DateGenerator
+{
+    public static class DateExtensions
+    {
+        public static List<DayOfWeek> DaysOfWeekFromString(string[] days)
+        {
+            var result = new List<DayOfWeek>();
+            foreach (var day in days)
+            {
+                var dayOfWeek = DayOfWeekFromString(day);
+                result.Add(dayOfWeek);
+            }
+            return result;
+        }
+
+        public static DayOfWeek DayOfWeekFromString(string day)
+        {
+            switch (day.ToLower())
+            {
+                case "monday":
+                    return DayOfWeek.Monday;
+                case "tuesday":
+                    return DayOfWeek.Tuesday;
+                case "wednesday":
+                    return DayOfWeek.Wednesday;
+                case "thursday":
+                    return DayOfWeek.Thursday;
+                case "friday":
+                    return DayOfWeek.Friday;
+                case "saturday":
+                    return DayOfWeek.Saturday;
+                case "sunday":
+                    return DayOfWeek.Sunday;
+
+                default:
+                    return DayOfWeek.Sunday;
+            }
+        }
+        public static DateFrequency FrequencyFromString(string frequency)
+        {
+            switch (frequency.ToLower())
+            {
+                case "weekly":
+                    return DateFrequency.Weekly;
+                case "biweekly":
+                    return DateFrequency.Biweekly;
+                case "monthly":
+                    return DateFrequency.Monthly;
+            }
+
+            return DateFrequency.None;
+        }
+
+    }
+}
---
+++ DateGenerator/DateGenerator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DateGenerator
+{
+    /// <summary>
+    /// Generates list of dates by selected days in week and frequency between startDate and endDate and stores them in 'Generated' property
+    /// </summary>
+    public class DateGenerator
+    {
+        private const int _daysInWeek = 7;
+
+        private DateTime StartDate { get; set; }
+        private DateTime EndDate { get; set; }
+        private DateFrequency Frequency { get; set; }
+        private List<DayOfWeek> Days { get; set; }
+        public List<DateTime> Generated { get; set; } = new List<DateTime>();
+
+
+        /// <summary>
+        /// Automatically generates Dates and stores them in 'Generated' property
+        /// </summary>
+        /// <param name="startDate">Start of generated sequence</param>
+        /// <param name="endDate">End of generated sequence</param>
+        /// <param name="days">Array of string days in week</param>
+        /// <param name="frequency">Accepted values: 'Weekly', 'Biweekly', 'Monthly'</param>
+        public DateGenerator(DateTime startDate, DateTime endDate, List<DayOfWeek> days, DateFrequency frequency)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            Days = days;
+            Frequency = frequency;
+        }
+
+        /// <summary>
+        /// Automatically generates Dates and stores them in 'Generated' property
+        /// </summary>
+        /// <param name="startDate">Start of generated sequence</param>
+        /// <param name="endDate">End of generated sequence</param>
+        /// <param name="days">Array of string days in week</param>
+        /// <param name="frequency">Accepted values: 'Weekly', 'Biweekly', 'Monthly'</param>
+        public DateGenerator(DateTime startDate, DateTime endDate, string[] days, string frequency)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            Days = DateExtensions.DaysOfWeekFromString(days);
+            Frequency = DateExtensions.FrequencyFromString(frequency);
+
+            Generate();
+        }
+
+        public void Generate()
+        {
+            foreach (var day in Days)
+            {
+                var daysInRange = GetDaysInRange(day);
+                var resultDays = FilterDaysByFrequency(daysInRange);
+                Generated.AddRange(resultDays);
+            }
+            FilterRemainderDates();
+            Generated = Generated.OrderByDescending(u => u.Date).ToList();
+        }
+
+        private void FilterRemainderDates()
+        {
+            Generated = Generated.Where(u => u <= EndDate).ToList();
+        }
+
+        private List<DateTime> FilterDaysByFrequency(List<DateTime> days)
+        {
+            switch (Frequency)
+            {
+                case DateFrequency.Weekly:
+                    return days;
+                case DateFrequency.Biweekly:
+                    return days.Where((x, i) => i % 2 == 0).ToList();
+                case DateFrequency.Monthly:
+                    var firstDay = days.First();
+                    var filtered = days.Where((x, i) => i % 4 == 0);
+                    filtered.Prepend(firstDay);
+                    return filtered.ToList();
+            }
+            return null;
+        }
+
+        private List<DateTime> GetDaysInRange(DayOfWeek day)
+        {
+            var localOffsetDay = StartDate;
+            var result = new List<DateTime>();
+            var daysToAdd = ((int)day - (int)localOffsetDay.DayOfWeek + _daysInWeek) % _daysInWeek;
+            do
+            {
+                localOffsetDay = localOffsetDay.AddDays(daysToAdd);
+                result.Add(localOffsetDay);
+                daysToAdd = _daysInWeek;
+            }
+            while (localOffsetDay < EndDate);
+
+            return result;
+        }
+    }
+
+    public enum DateFrequency
+    {
+        Weekly,
+        Biweekly,
+        Monthly,
+        None
+    }
+}
